b091c2a6fc6aa1fdc6bddfc3d58d0e9bf819946f
jcnimble <jcginelli@outlook.com>
2019-04-18 14:33:50 -0400

    Followed Homework instructions up to #6, created liri.js

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.28e4c4ffa9c94a6cbe809b17610be949,
+  secret: process.env.d9200dbf59fb41c9af55553da73a7790
+};
+
